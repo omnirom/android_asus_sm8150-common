d86ffbf1302498f70548451b8308305399851903
Bharath <bharath@teamb58.org>
2022-11-05 18:47:02 +0530

    sm8150-common: Drop AB_OTA_POSTINSTALL_CONFIG
    This is only for Q launch devices and with dynamic partitions
    enabled.
    
    Ref: https://git.codelinaro.org/clo/la/platform/vendor/qcom/titanium_64/-/commit/21d2b6fae18b0ac15b658be2c4ef7edcafdedee3#a46698751e55f0ad5f5cbef1a73bbe1f11ccdba9
    Change-Id: I21033c0a7e3b06582e7196f8dbcf7d09bb9acba5

--- common.mk
+++ common.mk
@@ -37,15 +37,6 @@ AB_OTA_PARTITIONS += \
     vbmeta \
     vendor
 
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/omnipreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-PRODUCT_PACKAGES += \
-    omnipreopt_script
-
 # Android Auto for Wireless
 ifeq ($(ROM_BUILDTYPE),$(filter $(ROM_BUILDTYPE),GAPPS))
 PRODUCT_PACKAGES += \
